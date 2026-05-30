93c5b4cc344fe2adb78872804a951e3780a58570
THẢO HIỀN <thaohien@MacBook-Air-cua-THAO.local>
2026-05-30 20:54:30 +0700

    fix ios firebase

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,56 +1,19 @@
-import Flutter
 import UIKit
-import UserNotifications
+import Flutter
+import FirebaseCore
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    UNUserNotificationCenter.current().delegate = self
-    application.registerForRemoteNotifications()
+    FirebaseApp.configure()
+    GeneratedPluginRegistrant.register(with: self)
 
-    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-    
-    let settingsChannel = FlutterMethodChannel(name: "com.huflit.smart_canteen/settings",
-                                              binaryMessenger: controller.binaryMessenger)
-    settingsChannel.setMethodCallHandler({
-      (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-      if call.method == "openSettings" {
-        if let url = URL(string: UIApplication.openSettingsURLString) {
-          if UIApplication.shared.canOpenURL(url) {
-            UIApplication.shared.open(url, options: [:], completionHandler: nil)
-          }
-        }
-        result(nil)
-      } else {
-        result(FlutterMethodNotImplemented)
-      }
-    })
-
-    let badgeChannel = FlutterMethodChannel(name: "com.huflit.smart_canteen/badge",
-                                            binaryMessenger: controller.binaryMessenger)
-    badgeChannel.setMethodCallHandler({
-      (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-      if call.method == "setBadge" {
-        if let count = call.arguments as? Int {
-          if #available(iOS 16.0, *) {
-            UNUserNotificationCenter.current().setBadgeCount(count) { error in
-              if let error = error {
-                print("Error setting badge: \(error)")
-              }
-            }
-          } else {
-            UIApplication.shared.applicationIconBadgeNumber = count
-          }
-        }
-        result(nil)
-      } else {
-        result(FlutterMethodNotImplemented)
-      }
-    })
-
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    return super.application(
+      application,
+      didFinishLaunchingWithOptions: launchOptions
+    )
   }
 }
